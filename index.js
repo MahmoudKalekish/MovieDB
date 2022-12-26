ec2876de87edb817c569bd7e8f39cf9e9ae9f05d
Mahmoud <mahmoudkalekish18@gmail.com>
2022-12-26 16:33:06 +0200

    step 4

--- index.js
+++ index.js
@@ -3,14 +3,26 @@ const app = express();
 
 const PORT = 3000;
 
-app.get('/test', (req, res) => {
-  res.json({status: 200, message: 'ok'});
+app.get('/hello/:id', (req, res) => {
+  const id = req.params.id;
+//   http://localhost:3000/hello/123
+  res.json({status: 200, message: `Hello, ${id || 'world'}`});
 });
 
-app.get('/time', (req, res) => {
-  const currentTime = new Date().toLocaleTimeString();
-  res.json({status: 200, message: currentTime});
+app.get('/hello', (req, res) => {
+  res.json({status: 200, message: 'Hello, world'});
 });
+
+app.get('/search', (req, res) => {
+    const search = req.query.s;
+    //http://localhost:3000/search?s=cats
+    if (search) {
+      res.status(200).json({status: 200, message: 'ok', data: search});
+    } else {
+      res.status(500).json({status: 500, error: true, message: 'you have to provide a search'});
+    }
+  });
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
